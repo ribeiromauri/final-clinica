fc99d415bf1742755a745b23c0bc6aec0dfecf5b
ribeiromauri <110779735+ribeiromauri@users.noreply.github.com>
2022-12-04 12:11:57 -0300

    arreglo deslogin

--- Clinica/MiMaster.Master.cs
+++ Clinica/MiMaster.Master.cs
@@ -4,14 +4,23 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Dominio;
+using Controlador;
 
 namespace Clinica
 {
     public partial class MiMaster : System.Web.UI.MasterPage
     {
+        public Usuarios user;
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void btnSalir_Click(object sender, EventArgs e)
+        {
+            Session.Abandon();
+            Response.Redirect("Login.aspx", true);
         }
     }
 }
